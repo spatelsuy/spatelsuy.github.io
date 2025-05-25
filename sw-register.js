63894abe0bcae74097b6dc3b9d9f40fff382bb4d
Sunil Patel <126328736+spatelsuy@users.noreply.github.com>
2025-05-25 13:22:52 -0400

    Create sw-register.js

---
+++ sw-register.js
@@ -0,0 +1,11 @@
+// sw-register.js
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(function (reg) {
+      console.log('Service Worker registered successfully.', reg);
+    })
+    .catch(function (err) {
+      console.error('Service Worker registration failed:', err);
+    });
+}
